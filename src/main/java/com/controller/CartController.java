be3d0c91d836ca0286edf3ef672da4108c0c80c2
Kishore <kishoresaravanan750@gmail.com>
2026-08-17 22:27:16 +0530

    Update CartController.java

--- src/main/java/com/controller/CartController.java
+++ src/main/java/com/controller/CartController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.model.Cart;
 import com.model.Customer;
